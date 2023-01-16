4f96d8b20b13f600b597bc0ffa917108baf3e1ec
Omyguasch <omyguasch@gmail.com>
2023-01-16 00:26:19 -0500

    updated prediction function
    You should check the structure of the data you are receiving from the new data source and update the code accordingly. For example, in the provided code, the 'c' array is being used to calculate the EMA and RSI, but the 'h' and 'l' arrays are not being used at all. You can use them to calculate other indicators or use them in the prediction step.
    
    You can add more conditions to the prediction step to make the prediction more accurate. For example, you can check for the slope of the EMA and RSI, or you can check for patterns in the data such as head and shoulders or flag patterns.
    
    You can also use other indicators in addition to EMA and RSI to make the prediction. For example, you can use the moving average convergence divergence (MACD) indicator or the stochastic oscillator indicator to confirm the prediction made by EMA and RSI.
    
    You can also try different period values for EMA and RSI to see which values work best for the specific data set.
    
    Finally, you should handle the errors that might occur in the API call, for example, if the API returns a status code other than 200, or if the response data is not in the expected format.

--- script.js
+++ script.js
@@ -30,28 +30,31 @@ const calculateRSI = (data, period) => {
 }
 
 const predict = async () => {
-  try {
-      // Fetch historical data from the API
-      const response = await axios.get(`https://finnhub.io/api/v1//crypto/candle?symbol=BINANCE:BTCUSDT&resolution=D&count=100&token=cf2c4u2ad3idqn4q5ba0cf2c4u2ad3idqn4q5bag`);
-      const data = response.data;
-      
-      
-      // Calculate EMA, RSI and volume
-      const ema = calculateEMA(data, 30);
-      const rsi = calculateRSI(data, 14);
-      const volume = data.v.reduce((acc, val) => acc + val, 0);
-      console.log(rsi, volume, ema)
-      // Make prediction based on the calculated values
-      if (rsi > 70 && ema > data.c[data.length - 1] && volume > 2000000) {
-          console.log("Next 30min wick is likely to be red");
-      } else if (rsi < 30 && ema < data.c[data.length - 1] && volume < 2000000) {
-          console.log("Next 30min wick is likely to be green");
-      } else {
-          console.log("Unable to predict next 30min wick");
-      }
-  } catch (error) {
-      console.log(error);
+    try {
+        // Fetch historical data from the API
+        const response = await axios.get(`https://finnhub.io/api/v1//crypto/candle?symbol=BINANCE:BTCUSDT&resolution=D&count=1&token=cf2c4u2ad3idqn4q5ba0cf2c4u2ad3idqn4q5bag`);
+        // check if the response data is in the expected format
+        if (!response.data || !response.data.c) {
+            console.log("Unexpected data format received from the API")
+            return
+        }
+        const data = response.data;
+        // Calculate EMA, RSI and other indicators
+        const ema = calculateEMA(data.c, 30);
+        const rsi = calculateRSI(data.c, 14);
+        // Add more conditions to make prediction
+        const slopeEma = (ema - data.c[data.c.length - 1])/30;
+        const slopeRsi = (rsi - calculateRSI(data.c, data.c.length - 1))/14;
+        // Make prediction based on the calculated values
+        if (rsi > 70 && slopeRsi > 0 && slopeEma > 0 && ema > data.c[data.c.length - 1]) {
+            console.log("Next 30min wick is likely to be red");
+        } else if (rsi < 30 && slopeRsi < 0 && slopeEma < 0 && ema < data.c[data.c.length - 1]) {
+            console.log("Next 30min wick is likely to be green");
+        } else {
+            console.log("Unable to predict next 30min wick");
+        }
+    } catch (error) {
+        console.log(error);
+    }
   }
-}
-
 predict();
