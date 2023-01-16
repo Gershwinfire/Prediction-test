28f5b097ba5dfe6880782d7433e6792cc9db604f
Omyguasch <omyguasch@gmail.com>
2023-01-16 00:59:00 -0500

    updated with a bollinger band function

--- script.js
+++ script.js
@@ -5,56 +5,69 @@ const INTERVAL = '30min';
 const SYMBOL = 'BTCUSDT';
 
 const calculateEMA = (data, period) => {
-    let k = 2 / (period + 1);
-    let ema = data.c[0];
-    for (let i = 1; i < data.c.length; i++) {
-        ema = data.c[i] * k + ema * (1 - k);
-    }
-    return ema;
+let k = 2 / (period + 1);
+let ema = data[0];
+for (let i = 1; i < data.length; i++) {
+ema = data[i] * k + ema * (1 - k);
+}
+return ema;
 }
 
 const calculateRSI = (data, period) => {
-    let gains = 0;
-    let losses = 0;
-    for (let i = 1; i < data.c.length; i++) {
-        let change = data.c[i] - data.c[i - 1];
-        if (change > 0) {
-            gains += change;
-        } else {
-            losses -= change;
-        }
-    }
-    let avgGain = gains / period;
-    let avgLoss = losses / period;
-    return 100 - (100 / (1 + (avgGain / avgLoss)));
+let gains = 0;
+let losses = 0;
+for (let i = 1; i < data.length; i++) {
+let change = data[i] - data[i - 1];
+if (change > 0) {
+gains += change;
+} else {
+losses -= change;
+}
+}
+let avgGain = gains / period;
+let avgLoss = losses / period;
+return 100 - (100 / (1 + (avgGain / avgLoss)));
+}
+
+const calculateBollingerBands = (data, period) => {
+const mean = data.reduce((a, b) => a + b) / data.length;
+let sd = 0;
+data.forEach(d => {
+sd += Math.pow(d - mean, 2);
+});
+sd = Math.sqrt(sd / data.length);
+const upperBand = mean + (2 * sd);
+const lowerBand = mean - (2 * sd);
+return {upperBand, lowerBand};
 }
 
 const predict = async () => {
-    try {
-        // Fetch historical data from the API
-        const response = await axios.get(`https://finnhub.io/api/v1//crypto/candle?symbol=BINANCE:BTCUSDT&resolution=D&count=1&token=cf2c4u2ad3idqn4q5ba0cf2c4u2ad3idqn4q5bag`);
-        // check if the response data is in the expected format
-        if (!response.data || !response.data.c) {
-            console.log("Unexpected data format received from the API")
-            return
-        }
-        const data = response.data;
-        // Calculate EMA, RSI and other indicators
-        const ema = calculateEMA(data.c, 30);
-        const rsi = calculateRSI(data.c, 14);
-        // Add more conditions to make prediction
-        const slopeEma = (ema - data.c[data.c.length - 1])/30;
-        const slopeRsi = (rsi - calculateRSI(data.c, data.c.length - 1))/14;
-        // Make prediction based on the calculated values
-        if (rsi > 70 && slopeRsi > 0 && slopeEma > 0 && ema > data.c[data.c.length - 1]) {
-            console.log("Next 30min wick is likely to be red");
-        } else if (rsi < 30 && slopeRsi < 0 && slopeEma < 0 && ema < data.c[data.c.length - 1]) {
-            console.log("Next 30min wick is likely to be green");
-        } else {
-            console.log("Unable to predict next 30min wick");
-        }
-    } catch (error) {
-        console.log(error);
+try {
+// Fetch historical data from the API
+const response = await axios.get("https://finnhub.io/api/v1/crypto/candle?symbol=BINANCE:BTCUSDT&resolution=30&count=24&token=cf2c4u2ad3idqn4q5ba0cf2c4u2ad3idqn4q5bag");
+// check if the response data is in the expected format
+if (!response.data || !response.data.c) {
+console.log("Unexpected data format received from the API")
+return
+}
+const data = response.data;
+// Calculate EMA, RSI, Bollinger Bands and other indicators
+const ema = calculateEMA(data.c, 30);
+const rsi = calculateRSI(data.c, 14);
+const bollingerBands = calculateBollingerBands(data.c, 14);
+// Add more conditions to make prediction
+const slopeEma = (ema - data.c[data.c.length - 1])/30;
+const slopeRsi = (rsi - calculateRSI(data.c, data.c.length - 1))/14;
+// Make prediction based on the calculated values
+if (rsi > 50 && slopeRsi > 0 && slopeEma > 0 && ema > data.c[data.c.length-1] && ema > bollingerBands.upperBand) {
+    console.log("Next 30min wick is likely to be red");
+    } else if (rsi < 40 && slopeRsi < 0 && slopeEma < 0 && ema < data.c[data.c.length - 1] && ema < bollingerBands.lowerBand) {
+    console.log("Next 30min wick is likely to be green");
+    } else {
+    console.log("Unable to predict next 30min wick");
     }
-  }
-predict();
+    } catch (error) {
+    console.log(error);
+    }
+    }
+    predict();
